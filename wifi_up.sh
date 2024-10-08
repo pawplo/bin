cac4fa50c931e1b8b55e4f8ba79ec13851f6d4f6
Paweł Płóciennik <paplociennik@gmail.com>
2024-10-08 15:41:28 +0200

    wifi_up.sh

--- wifi_up.sh
+++ wifi_up.sh
@@ -2,17 +2,19 @@
 
 if [ -f ~/.openwrt ]; then
 
-    if [ `wifi status radio0 | grep "\"up\": false"` ]; then
-        echo "`date` [0]" >> /var/log/wifi_up
-        wifi
-        /root/beep.sh
-    fi
+w=`wifi status radio0 | grep "\"up\": false"`
+if [ "$w" ]; then
+    echo "`date` [0]" >> /var/log/wifi_up
+    wifi
+    /root/beep.sh
+fi
 
-    if [ `wifi status radio1 | grep "\"up\": false"` ]; then
-        echo "`date` [1]" >> /var/log/wifi_up
-        wifi
-        /root/beep.sh
-    fi
+w=`wifi status radio1 | grep "\"up\": false"`
+if [ "$w" ]; then
+    echo "`date` [1]" >> /var/log/wifi_up
+    wifi
+    /root/beep.sh
+fi
 
 else
     echo "???"
